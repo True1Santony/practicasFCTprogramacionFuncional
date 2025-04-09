1e0a4e68a7bc2cd481398f2f2b342f7e3a6cd8f6
Espartaco Nassekine Local <espartaconassekine@gmail.com>
2025-04-09 13:47:56 +0200

    Tratamiento y transformacion de stream.

--- src/main/java/com/practica/funcional/Main.java
+++ src/main/java/com/practica/funcional/Main.java
@@ -1,8 +1,8 @@
 package com.practica.funcional;
 
-import java.util.List;
+import com.practica.funcional.v10_optional.Descripcion;
+
 import java.util.Random;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
@@ -10,45 +10,19 @@ public class Main {
 
     Random random = new Random();
     public Main(){
-      /*  List<String> nombres = Stream.of("Manolo", "espartaco", "Sandra", "Amanda", "Marian")
-                .filter(nombre -> nombre.contains("o"))
-                .limit(1)
-                .toList();
-        System.out.println(nombres);*/
-/*
-        List<Integer> resultado = Stream.generate(() -> {
-            int next = random.nextInt(10);
-            System.out.printf("Se ha generado el %d\n", next);
-            return next;
-                }).limit(20)//Ejecucion lazy, llama las veces que se le limite si no, infinito
-                .toList();
-        System.out.println(resultado);
+      random.ints(10, 1,11)
+              .boxed()
+              .filter(valor -> valor >= 5)
+              .sorted(Integer::compareTo)
+              .map(valor -> valor * valor)
+              .map(Descripcion::new)
+//              .peek(System.out::println)
+              .map(Descripcion::getValue)
+              .forEach(valor -> System.out.print(valor + "  "));
+//              .reduce(0, Integer::sum);
 
-        //valor inicial y una funcion unaria que determina como va a operar el sigiente, se agrega una condicion (valor, predicado, funcion unaria)
-        List<Integer> resultadoIterado = Stream.iterate(1, valor -> valor <1000,valor -> valor * 2)
-                .toList();
-        System.out.println(resultadoIterado);
 
-        List<Integer> resultadoIterado1 = random.ints(10,0,10)
-                .boxed()//transformacion al wraper correspondiente, adecuado.
-                .toList();
-        System.out.println(resultadoIterado1);
 
-        //todos los valores de inicio a  fin 
-        List<Integer> resultadoIterado2 = IntStream.rangeClosed(0,10)
-                .boxed()
-                .toList();
-        System.out.println(resultadoIterado2);
-        */
-
-        //.forEach(System.out::println)
-        //.map(this::getRandomNumbers)
-        //.forEach(integerStream -> integerStream.forEach(System.out::println))
-        List<Integer> resultado1 = Stream.of(2, 4, 6)
-                .flatMap(this::getRandomNumbers)//aplana los 3 stream generados en uno, asi puedo recorrer con foreach e introducirlo en Collections
-                .toList();
-
-        System.out.println(resultado1);
     }
 
     private Stream<Integer> getRandomNumbers(Integer size) {
