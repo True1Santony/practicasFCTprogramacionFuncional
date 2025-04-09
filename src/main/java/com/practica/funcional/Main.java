4136b9e1868cea2d8aa4f28084b87c812d68324c
Espartaco Nassekine Local <espartaconassekine@gmail.com>
2025-04-09 12:04:01 +0200

    Stream, sus metodos y usos.

--- src/main/java/com/practica/funcional/Main.java
+++ src/main/java/com/practica/funcional/Main.java
@@ -1,50 +1,61 @@
 package com.practica.funcional;
 
-import com.practica.funcional.v10_optional.Descripcion;
-import com.practica.funcional.v10_optional.Flujo;
-import com.practica.funcional.v10_optional.NumbersUtils;
-
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
 
 public class Main {
 
     Random random = new Random();
     public Main(){
-        ArrayList<String> nombres = new ArrayList<>(List.of("Manolo", "espartaco", "Sandra", "Amanda", "Marian"));
-       // nombres.forEach(System.out::println); // como un for normal.
-
-        //1. Funcion generadora de stream.
-        nombres.stream()
-                //2. 0 o mas operacione intermedias
+      /*  List<String> nombres = Stream.of("Manolo", "espartaco", "Sandra", "Amanda", "Marian")
                 .filter(nombre -> nombre.contains("o"))
-               // .map(nombre -> nombre.length())
-                //3. operacion terminal
-                .forEach(System.out::println);
-
-        //Resumen: Producir, procesar y consumir.
+                .limit(1)
+                .toList();
+        System.out.println(nombres);*/
 /*
-        Flujo.proveer(10, this::randomInt)
-                .filtrar(valor -> valor >= 0)//NoSuchElementException, lista vacia, si se filtra por un numero superior al generado por random (10)
-                .ordenar(Integer::compareTo)
-                .transformar(NumbersUtils::elevarAlCuadrado)
-                .transformar(Descripcion::new)
-                .actuar(System.out::println)
-                .transformar(Descripcion::getValue)
-                .max(Comparator.naturalOrder())
-                .ifPresentOrElse(
-                        valor -> System.out.println("Maximo: " + valor.doubleValue()),
-                        ()-> System.out.println("No hay maximo porque el flujo esta vacio")
-                );
-                */
+        List<Integer> resultado = Stream.generate(() -> {
+            int next = random.nextInt(10);
+            System.out.printf("Se ha generado el %d\n", next);
+            return next;
+                }).limit(20)//Ejecucion lazy, llama las veces que se le limite si no, infinito
+                .toList();
+        System.out.println(resultado);
 
+        //valor inicial y una funcion unaria que determina como va a operar el sigiente, se agrega una condicion (valor, predicado, funcion unaria)
+        List<Integer> resultadoIterado = Stream.iterate(1, valor -> valor <1000,valor -> valor * 2)
+                .toList();
+        System.out.println(resultadoIterado);
+
+        List<Integer> resultadoIterado1 = random.ints(10,0,10)
+                .boxed()//transformacion al wraper correspondiente, adecuado.
+                .toList();
+        System.out.println(resultadoIterado1);
+
+        //todos los valores de inicio a  fin 
+        List<Integer> resultadoIterado2 = IntStream.rangeClosed(0,10)
+                .boxed()
+                .toList();
+        System.out.println(resultadoIterado2);
+        */
+
+        //.forEach(System.out::println)
+        //.map(this::getRandomNumbers)
+        //.forEach(integerStream -> integerStream.forEach(System.out::println))
+        List<Integer> resultado1 = Stream.of(2, 4, 6)
+                .flatMap(this::getRandomNumbers)//aplana los 3 stream generados en uno, asi puedo recorrer con foreach e introducirlo en Collections
+                .toList();
+
+        System.out.println(resultado1);
+    }
+
+    private Stream<Integer> getRandomNumbers(Integer size) {
+        return random.ints(size, 0, 10).boxed();
     }
 
 
     public static void main(String[] args) {new Main();}
 
-    private int randomInt(){ return random.nextInt(10);}
 }
